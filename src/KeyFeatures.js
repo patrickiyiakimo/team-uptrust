698eedd96f07cc937b8ef494aee85a8c11538303
Patrick Iyiakimo <iyiakimopatrick2002@gmail.com>
2024-03-07 10:11:42 -0800

    Update KeyFeatures.js

--- src/KeyFeatures.js
+++ src/KeyFeatures.js
@@ -10,28 +10,28 @@ const KeyFeatures = () => {
       title: "Job Description Analyser",
       body: "Uncover the secrets behind job postings effortlessly. Paste your dream job description, and let Uptrust extract the key skills and requirements, giving you a clear roadmap to success.",
       text: "Job Description Analyser",
-      icon: <FiArrowRight size={20} color="white" />,
+      // icon: <FiArrowRight size={20} color="white" />,
       id: 1,
     },
     {
       title: "Profile Comparison",
       body: "Discover how you measure up to your dream job. Our profile comparison tool analysis your skills against job requirements and guiding you on the path to becoming the perfect candidate.",
       text: "Profile Comparison",
-      icon: <FiArrowRight size={20} color="white" />,
+      // icon: <FiArrowRight size={20} color="white" />,
       id: 2,
     },
     {
       title: "Learning Recommender",
       body: "Embark on a personalised learning journey. We recommend courses tailored to your unique skill gaps with seemless integrationwith YouTube, Udemy, and Coursera.",
       text: "Learning Recommender",
-      icon: <FiArrowRight size={20} color="white" />,
+      // icon: <FiArrowRight size={20} color="white" />,
       id: 3,
     },
     {
       title: "Profile Builder",
       body: "Craft a standout profile that reflects your true potential. Our profile builder captures your background, interests, and goals, shaping a profile that speaks to your strengths.",
       text: "Profile Builder",
-      icon: <FiArrowRight size={20} color="white" />,
+      // icon: <FiArrowRight size={20} color="white" />,
       id: 4,
     },
   ]);
@@ -44,7 +44,7 @@ const KeyFeatures = () => {
         <div className="detailers" key={detail.id}>
           <h1>{detail.title}</h1>
           <section>{detail.body}</section>
-          <strong>{detail.icon}</strong>
+          {/* <strong>{detail.icon}</strong> */}
           <p>{detail.text}</p>
         </div>
       ))}
