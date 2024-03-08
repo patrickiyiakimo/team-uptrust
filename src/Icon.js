611764be8ca8383f4e2d42207240dddd807b2aae
Patrick Iyiakimo <iyiakimopatrick2002@gmail.com>
2024-03-08 15:00:44 -0800

    Update Icon.js

--- src/Icon.js
+++ src/Icon.js
@@ -2,15 +2,45 @@ const Edit = () => {
     return (
       <div className="icon">
         <h1>Edit Education</h1>
-        <input type="text" name="text" placeholder="Name Of School" /> <br />
-        <input type="text" name="text" placeholder="Degree" /> <br />
-        <input type="text" name="text" placeholder="Institution" /> <br />
-        <input type="text" name="text" placeholder="Field Of Study" /> <br />
-        <input type="text" name="text" placeholder="Start Year" /> <br />
-        <input type="text" name="text" placeholder="End Year" /> <br />
-        <input type="text" name="text" placeholder="Institution Website" />{" "}
+        <input
+          type="text"
+          name="text"
+          placeholder="Name Of School"
+          required
+        />
         <br />
-        <input type="text" name="text" placeholder="Description" /> <br />
+        <input type="text" name="text" placeholder="Degree" required /> <br />
+        <input
+          type="text"
+          name="text"
+          placeholder="Institution"
+          required
+        />
+        <br />
+        <input
+          type="text"
+          name="text"
+          placeholder="Field Of Study"
+          required
+        />
+        <br />
+        <input type="text" name="text" placeholder="Start Year" required />{" "}
+        <br />
+        <input type="text" name="text" placeholder="End Year" required /> <br />
+        <input
+          type="text"
+          name="text"
+          placeholder="Institution Website"
+          required
+        />
+        <br />
+        <input
+          type="text"
+          name="text"
+          placeholder="Description"
+          required
+        />
+        <br />
         <button>Submit</button>
       </div>
     );
