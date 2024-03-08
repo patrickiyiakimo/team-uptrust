2b9e8374d50d2ec03dcefb97a124e959efdcde4a
Patrick Iyiakimo <iyiakimopatrick2002@gmail.com>
2024-03-07 18:34:28 -0800

    Update Profile.js

--- src/Profile.js
+++ src/Profile.js
@@ -28,8 +28,8 @@ const Profile = () => {
           />
         </svg>
       </button>
+      <strong>Patrick Iyiakimo</strong>
       <div className="section">
-        <strong>Patrick Iyiakimo</strong>
         <span>
           <svg
             xmlns="http://www.w3.org/2000/svg"
