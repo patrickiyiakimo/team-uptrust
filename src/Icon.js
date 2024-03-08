c2257bbf9f540542a2620da50272659fb2fec0a7
Patrick Iyiakimo <iyiakimopatrick2002@gmail.com>
2024-03-08 14:18:01 -0800

    updated

--- src/Icon.js
+++ src/Icon.js
@@ -1,9 +1,19 @@
 const Edit = () => {
     return (
-        <div className="edit">
-            <h1>hello editor:)</h1>
-        </div>
-     );
+      <div className="icon">
+        <h1>Edit Education</h1>
+        <input type="text" name="text" placeholder="Name Of School" /> <br />
+        <input type="text" name="text" placeholder="Degree" /> <br />
+        <input type="text" name="text" placeholder="Institution" /> <br />
+        <input type="text" name="text" placeholder="Field Of Study" /> <br />
+        <input type="text" name="text" placeholder="Start Year" /> <br />
+        <input type="text" name="text" placeholder="End Year" /> <br />
+        <input type="text" name="text" placeholder="Institution Website" />{" "}
+        <br />
+        <input type="text" name="text" placeholder="Description" /> <br />
+        <button>Submit</button>
+      </div>
+    );
 }
  
 export default Edit;
