99ddbe41e4bb208c7228290bfbc600d2bad6da74
Patrick Iyiakimo <iyiakimopatrick2002@gmail.com>
2024-03-08 17:55:09 -0800

    Update Experience.js

--- src/Experience.js
+++ src/Experience.js
@@ -4,7 +4,7 @@ const Experieence = () => {
     return (
       <div className="experience">
         <div className="section-two">
-          <RouterLink to="experience">
+          <RouterLink to="experiences">
             <span>
               <svg
                 xmlns="http://www.w3.org/2000/svg"
